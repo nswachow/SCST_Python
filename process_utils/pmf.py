2239b8a6f00b9706c04eccac1595af8717090906
Neil Wachowski <nwachowski@gmail.com>
2019-07-28 18:59:43 -0600

    PMF classes are now ContiguousPMF classes.
    These classes represent PMF of RV with realizations that line on a continuous interval, where each value has an associated minimum probability.

--- process_utils/pmf.py
+++ process_utils/pmf.py
@@ -1,32 +1,43 @@
 import numpy as np
+import scipy.stats
 from collections import defaultdict
-from scipy.stats import entropy
-from typing import Tuple
+from typing import Tuple, Dict
 
 
-class PMF:
+class ContiguousPMF:
     '''
-    Implements a probability mass function that can be used to obtain probabilities when called with
-    a value.
+    Implements a probability mass function for a discrete random variable whose realizations are
+    integers on the interval [0, ``max_value``] where ``max_value`` is an input. The user also
+    specifies a minimum probability for all integers in the interval, such that the probability
+    will be at least this large for all values, even those that don't appear in the array of
+    training data.
     '''
 
-    def __init__(self, value_array: np.ndarray):
+    def __init__(self, train_array: np.ndarray, min_prob: float, max_value: int):
         '''
-        :param value_array: Array of realizations that this PMF represents, from which the mass
+        :param train_array: Array of realizations that this PMF represents, from which the mass
             function shall be computed.
+        :param min_prob: The minimum probabilty assigned to each value \in [0, max_value].
+        :param max_value: The maximum value associated with this PMF, which needs to be specified
+            since train_array may not contain any realizations of this values, or others on the
+            relevant interval.
         '''
 
-        prob_per_value = 1 / len(value_array)
-        self._prob_dict = defaultdict(lambda: 0.0)  # zero probability where there is no mass
+        assert min_prob >= 0
+        assert min_prob < 1 / max_value, "Cannot satisfy min_prob criterion"
+        assert max_value >= 0
 
-        for value in value_array:
-            self._prob_dict[value] += prob_per_value
+        train_array = train_array.astype(int)
+        assert np.all(train_array <= max_value)
+        assert np.all(train_array >= 0)
 
-        # Convert to a normal dict so we don't add every value we try from this point forward
-        self._prob_dict = dict(self._prob_dict)
+        value_array = np.arange(max_value + 1)
+        self._prob_dict = ContiguousPMF._getProbabilityDict(train_array, value_array, min_prob)
+        self._max_value = max_value
+
         self._entropy = None  # Compute lazily
 
-    def __call__(self, value: float) -> float:
+    def __call__(self, value: int) -> float:
         '''
         Evaluate the PMF at the input value.
 
@@ -34,10 +45,16 @@ class PMF:
 
         :return: Probability of the input value.
         '''
-        try:
-            return self._prob_dict[value]
-        except KeyError:
-            return 0.0
+        # We purposely want to throw a key error when using an invalid or out of range key
+        return self._prob_dict[value]
+
+    @property
+    def max_value(self) -> int:
+        '''
+        The maximum value associated with this PMF, such that this PMF only has non-zero mass for
+        values >= 0 and <= max_value.
+        '''
+        return self._max_value
 
     @property
     def entropy(self) -> float:
@@ -46,33 +63,86 @@ class PMF:
         '''
 
         if self._entropy is None:
-            self._entropy = entropy(list(self._prob_dict.values()), base=2)
+            non_zero_values = np.array(list(self._prob_dict.values()))
+            non_zero_values = non_zero_values[np.nonzero(non_zero_values)[0]]
+            self._entropy = scipy.stats.entropy(non_zero_values, base=2)
 
         return self._entropy
 
-
-class JointPMF(PMF):
-    '''
-    Implements a joint probability mass function that can be used to obtain probabilities when
-    called with a pair of values.
-    '''
-
-    def __init__(self, x_array: np.ndarray, y_array: np.ndarray):
+    @staticmethod
+    def _getProbabilityDict(train_array: np.ndarray, value_array: np.ndarray,
+                            min_prob: float) -> Dict[int, float]:
         '''
-        :param x_array, y_array: Arrays of realizations for the first and second dimension that
-            this joint PMF represents, from which the mass function shall be computed. Elements of
-            each array have one-to-one correspondance, i.e., element ``ii`` of each array are
-            assumed to have been realized simultaneously. These arrays must have the same length.
+        Helper function to allow for generating probabilites for this class and derived classes
         '''
 
-        assert len(x_array) == len(y_array)
+        prob_dict = defaultdict(lambda: 0.0)  # zero probability where there is no mass
+        prob_per_value = 1 / len(train_array)
+        for value in train_array:
+            prob_dict[value] += prob_per_value
 
-        tuple_array = [(x, y) for x, y in zip(x_array, y_array)]
-        super().__init__(tuple_array)
+        ContiguousPMF._enforceMinProb(prob_dict, value_array, min_prob)
 
-        self._x_marginal = PMF(x_array)
-        self._y_marginal = PMF(y_array)
+        # Convert to a normal dict so we don't add every value we try from this point forward
+        return dict(prob_dict)
 
+    @staticmethod
+    def _enforceMinProb(prob_dict: defaultdict, value_array: np.ndarray,
+                        min_prob: float) -> None:
+        '''
+        Enforce minimum probability constraint by setting probabilities < min_prob to min_prob
+        and rescaling the remaining probabilties so the mass sums to one.
+        '''
+
+        values_to_normalize = []
+        num_small_prob = 0
+        norm_mass = 0.0
+
+        # Run through this process even if min_prob == 0 so that probabilities are generated for all
+        # the possible values.
+        for value in value_array:
+            prob = prob_dict[value]
+            if prob < min_prob:
+                prob_dict[value] = min_prob
+                num_small_prob += 1
+            else:
+                values_to_normalize.append(value)
+                norm_mass += prob
+
+        if num_small_prob > 0:
+            prob_scale = (1 - num_small_prob * min_prob) / norm_mass
+            for value in values_to_normalize:
+                prob_dict[value] *= prob_scale
+
+
+class JointContiguousPMF(ContiguousPMF):
+    '''
+    Implements a joint probability mass function that functions similarly to ContiguousPMF, but
+    represents the joint distribution of two random variables. Can be used to obtain probabilities
+    when called with a pair of values. Minimum probabilities aren't enforced in this case, since it
+    causes discrepencies between the marginal and joint distributions.
+    '''
+
+    def __init__(self, x_train_array: np.ndarray, y_train_array: np.ndarray, max_value: int):
+        '''
+        :param x_train_array, y_train_array: Arrays of realizations for the first and second
+            dimension that this joint PMF represents, from which the mass function shall be
+            computed. Elements of each array have one-to-one correspondance, i.e., element ``ii``
+            of each array are assumed to have been realized simultaneously. These arrays must have
+            the same length.
+        :param max_value: See ContiguousPMF.__init__.
+        '''
+
+        assert len(x_train_array) == len(y_train_array)
+        self._x_marginal = ContiguousPMF(x_train_array, 0, max_value)
+        self._y_marginal = ContiguousPMF(y_train_array, 0, max_value)
+
+        train_array = [(x, y) for x, y in zip(x_train_array.astype(int), y_train_array.astype(int))]
+        value_array = [(x, y) for x in np.arange(max_value + 1) for y in np.arange(max_value + 1)]
+        self._prob_dict = ContiguousPMF._getProbabilityDict(train_array, value_array, 0)
+        self._max_value = max_value
+
+        self._entropy = None  # Compute lazily
         self._mutual_information = None  # Compute lazily
 
     @property
@@ -88,44 +158,73 @@ class JointPMF(PMF):
         return self._mutual_information
 
 
-class ConditionalPMF:
+class ConditionalContiguousPMF:
     '''
     Implements a conditional probability mass function that can be used to obtain probabilities when
     called with a tuple of dependent values (to determine the PMF to draw from) and a value at which
     to evaluate the probability.
     '''
 
-    def __init__(self, value_array: np.ndarray, depend_matrix: np.ndarray):
+    def __init__(self, train_array: np.ndarray, depend_matrix: np.ndarray, min_prob: float,
+                 max_value: int):
         '''
-        :param value_array: Array of realizations that this PMF represents, from which the mass
+        :param train_array: Array of realizations that this PMF represents, from which the mass
             function shall be computed.
         :param depend_matrix: {ii}th column represents the values of the dependent variables
-            when the {ii} element of ``value_array`` was realized. This is a matrix to enforce the
+            when the {ii} element of ``train_array`` was realized. This is a matrix to enforce the
             same number of dependent values per realization.
+        :param min_prob, max_value: See ContiguousPMF.__init__.
         '''
 
-        assert len(value_array) == depend_matrix.shape[1]
+        assert len(train_array) == depend_matrix.shape[1]
 
         # Construct a value array for each dependent value
         value_dict = defaultdict(list)
-        for ii in range(len(value_array)):
-            value_dict[tuple(depend_matrix[:, ii])].append(value_array[ii])
+        for ii in range(len(train_array)):
+            value_dict[tuple(depend_matrix[:, ii])].append(train_array[ii])
 
         # Construct a PMF for each dependent value
         self._pmf_dict = {}
         for depend_values, pmf_value_list in value_dict.items():
-            self._pmf_dict[depend_values] = PMF(np.array(pmf_value_list))
+            self._pmf_dict[depend_values] = ContiguousPMF(np.array(pmf_value_list), min_prob,
+                                                          max_value)
+
+        self._max_value = max_value
+        self._num_depend = depend_matrix.shape[0]
+        self._uniform_prob = 1 / max_value
+
+    @property
+    def max_value(self) -> int:
+        '''
+        The maximum value associated with this PMF, such that this PMF only has non-zero mass for
+        values >= 0 and <= max_value.
+        '''
+        return self._max_value
+
+    @property
+    def num_depend(self) -> int:
+        '''
+        The number of dependent values needed to calculate a conditional probability.
+        '''
+        return self._num_depend
 
     def __call__(self, value: float, depend_values: Tuple[float]) -> float:
         '''
-        Evaluate the conditional PMF according to the inputs.
+        Evaluate the conditional PMF according to the inputs. Return a uniform probability on the
+        interval [0, max_value] if there is no PMF associated with the input ``depend_values``.
 
         :param value: Realization value.
         :param depend_values: Values the PMF is conditioned on.
 
         :return: Conditional probability of the ``value`` input given the ``depend_values``.
         '''
+
+        # Enforce structure on depend_values
+        assert len(depend_values) == self._num_depend
+        depend_array = np.array(depend_values)
+        assert np.all(depend_array >= 0) and np.all(depend_array <= self._max_value)
+
         try:
             return self._pmf_dict[depend_values](value)
         except KeyError:
-            return 0.0
+            return self._uniform_prob
--- process_utils/test/test_pmf.py
+++ process_utils/test/test_pmf.py
@@ -2,73 +2,88 @@ import numpy as np
 from scipy.stats import entropy
 from functools import partial
 
-from process_utils.pmf import PMF, JointPMF, ConditionalPMF
+from process_utils.pmf import ContiguousPMF, JointContiguousPMF, ConditionalContiguousPMF
 from test_tools import exceptionTest
 
 
 def testPMF():
 
-    LOW = 0
-    HIGH = 10
+    HIGH = 5
+    MIN_PROB = 1e-4
 
-    value_array = np.random.randint(low=LOW, high=HIGH, size=100)
-    pmf = PMF(value_array)
+    value_array = np.random.randint(low=0, high=HIGH, size=100)
+    pmf = ContiguousPMF(value_array, MIN_PROB, HIGH)
 
     # Ensure probabilities sum to 1.0 and entropy is correctly calculated
     total_prob = 0.0
     entropy = 0.0
-    for value in np.arange(LOW, HIGH):
+    for value in np.arange(pmf.max_value+1):
         prob = pmf(value)
+        assert prob > 0
         total_prob += prob
         entropy -= prob * np.log2(prob)
 
     assert np.isclose(total_prob, 1.0)
     assert np.isclose(entropy, pmf.entropy)
 
+    exceptionTest(lambda: pmf(-1), KeyError)
+
+    # Should fail to create ContiguousPMF with invalid inputs
+    exceptionTest(partial(ContiguousPMF, train_array=np.arange(1), min_prob=-1, max_value=HIGH),
+                  AssertionError)
+    exceptionTest(partial(ContiguousPMF, train_array=np.arange(1), min_prob=1, max_value=-1),
+                  AssertionError)
+    exceptionTest(partial(ContiguousPMF, train_array=np.arange(5), min_prob=1e-4, max_value=1),
+                  AssertionError)
+
 
 def testJointPMF():
 
-    LOW = 0
-    HIGH = 10
+    HIGH = 5
 
-    x_array = np.random.randint(low=LOW, high=HIGH, size=100)
-    y_array = np.random.randint(low=LOW, high=HIGH, size=100)
+    x_array = np.random.randint(low=0, high=HIGH, size=100)
+    y_array = np.random.randint(low=0, high=HIGH, size=100)
 
-    joint_pmf = JointPMF(x_array, y_array)
-    x_pmf = PMF(x_array)
-    y_pmf = PMF(y_array)
+    joint_pmf = JointContiguousPMF(x_array, y_array, HIGH)
+    x_pmf = ContiguousPMF(x_array, 0, HIGH)
+    y_pmf = ContiguousPMF(y_array, 0, HIGH)
+    tuple_array = [(x, y) for x in np.arange(HIGH+1) for y in np.arange(HIGH+1)]
 
-    tuple_array = [(x, y) for x, y in zip(x_array, y_array)]
-    unique_tuples = list(set(tuple_array))
-
-    # Ensure probabilities sum to 1.0 and mutual information is correctly calculated
+    # Ensure probabilities sum to 1.0 and properties are correctly calculated
     total_prob = 0.0
+    entropy = 0.0
     mi = 0.0
-    for value_tuple in unique_tuples:
+    for value_tuple in tuple_array:
         joint_prob = joint_pmf(value_tuple)
+        assert joint_prob >= 0
         x_prob = x_pmf(value_tuple[0])
         y_prob = y_pmf(value_tuple[1])
 
         total_prob += joint_prob
-        mi += joint_prob * np.log2(joint_prob / (x_prob * y_prob))
+
+        if joint_prob > 0:
+            entropy -= joint_prob * np.log2(joint_prob)
+            if x_prob > 0 and y_prob > 0:
+                mi += joint_prob * np.log2(joint_prob / (x_prob * y_prob))
 
     assert np.isclose(total_prob, 1.0)
+    assert np.isclose(entropy, joint_pmf.entropy)
     assert np.isclose(mi, joint_pmf.mutual_information)
 
-    # Should fail to create JointPMF with invalid inputs
-    exceptionTest(partial(JointPMF, x_array=np.arange(1), y_array=np.arange(2)),
-                  AssertionError)
+    # Should fail to create JointContiguousPMF with invalid inputs
+    exceptionTest(partial(JointContiguousPMF, x_train_array=np.arange(1),
+                          y_train_array=np.arange(2), max_value=HIGH), AssertionError)
 
 
 def testConditionalPMF():
 
-    LOW = 0
     HIGH = 3
     NUM_DEPEND = 3
+    MIN_PROB = 1e-4
 
-    value_array = np.random.randint(low=LOW, high=HIGH, size=100)
-    depend_matrix = np.random.randint(low=LOW, high=HIGH, size=(NUM_DEPEND, 100))
-    pmf = ConditionalPMF(value_array, depend_matrix)
+    value_array = np.random.randint(low=0, high=HIGH, size=100)
+    depend_matrix = np.random.randint(low=0, high=HIGH, size=(NUM_DEPEND, 100))
+    pmf = ConditionalContiguousPMF(value_array, depend_matrix, MIN_PROB, HIGH)
 
     unique_depend = list(set([tuple(depend_matrix[:, ii])
                               for ii in range(depend_matrix.shape[1])]))
@@ -76,11 +91,17 @@ def testConditionalPMF():
     # Ensure probabilities sum to 1.0 for each dependent variable tuple
     for depend_tuple in unique_depend:
         total_prob = 0.0
-        for value in np.arange(LOW, HIGH):
+        for value in np.arange(pmf.max_value+1):
             prob = pmf(value, depend_tuple)
             total_prob += prob
 
         assert np.isclose(total_prob, 1.0)
+
+    # Test with untrained and invalid dependencies
+    assert np.isclose(pmf(0, (HIGH, HIGH, HIGH)),  1 / HIGH)
+    exceptionTest(lambda: pmf(0, (-1, 0, 0)), AssertionError)
+    exceptionTest(lambda: pmf(0, (HIGH+1, 0, 0)), AssertionError)
+    exceptionTest(lambda: pmf(0, (0,)), AssertionError)
 
 
 if __name__ == '__main__':
